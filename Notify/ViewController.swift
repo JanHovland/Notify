1dcf396857866d0556d728f3d6bb28f34d1e9f9d
Jan Hovland <jan.hovland@lyse.net>
2019-04-24 19:25:54 +0200

    Initial Commit

---
+++ Notify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notify
+//
+//  Created by Jan Hovland on 24/04/2019.
+//  Copyright © 2019 Jan Hovland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
